7ab55bbcd45073e53e1ca07c7928a91cbd377697
Keshav Kundan <keshavkundan.rnpw@gmail.com>
2023-02-12 11:25:39 +0530

    new file

---
+++ sophie/sophie.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    import pyttsx3
+    import speech_recognition as speechRecog
+    from pywhatkit.whats import sendwhatmsg_to_group_instantly
+
+    sendwhatmsg_to_group_instantly("FCtof9PDwXbKsXVWtLzP85", "Hi!", 15, True)
+    pass
