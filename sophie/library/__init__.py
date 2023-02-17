be6a562fef2663b77cb4cb6b8e4b271189244a4b
Keshav Kundan <keshavkundan.rnpw@gmail.com>
2023-02-17 06:53:01 +0530

    modified

--- sophie/library/__init__.py
+++ sophie/library/__init__.py
@@ -1,5 +1,6 @@
 
 import sys
 import time
+import random
 import datetime
 import urllib.request
--- sophie/sophie.py
+++ sophie/sophie.py
@@ -5,20 +5,28 @@ if __name__ == "__main__":
     from packages import pyttsx3
     from packages import speech_recognition as speechRecog
 
-    # def onStartTTS(name):
-    #     print('starting', name)
-    # def onWordTTS(name, location, length):
-    #     print('word', name, location, length)
-    # def onFinishTTS(name, completed):
-    #     print('finishing', name, completed)
+    language = (
+        ('English U.S.', 'english-us', 'english-us+f3', 'en-US'),
+        ('English India', 'english', 'english+f2', 'en-IN')
+    )
+
+    def onStartTTS(name):
+        # print('starting', name)
+        pass
+    def onWordTTS(name, location, length):
+        # print('word', name, location, length)
+        pass
+    def onFinishTTS(name, completed):
+        # print('finishing', name, completed)
+        pass
 
     try:
         engine = pyttsx3.init('espeak')
-        # engine.connect('started-utterance', onStartTTS)
-        # engine.connect('started-word', onWordTTS)
-        # engine.connect('finished-utterance', )onFinishTTS
-        engine.setProperty('rate', 132) # Set Rate
-        engine.setProperty('voice', 'english-us') # Set Voice
+        engine.connect('started-utterance', onStartTTS)
+        engine.connect('started-word', onWordTTS)
+        engine.connect('finished-utterance', onFinishTTS)
+        engine.setProperty('rate', 120) # Set Rate
+        engine.setProperty('voice', language[1][2]) # Set Voice
         engine.setProperty('volume', 1.0) # Set Volume
         def speak(text):
             engine.say(text)
@@ -56,7 +64,7 @@ if __name__ == "__main__":
                 audio = r.listen(source, 5, 5)
                 try:
                     print("Got it! Now to recognize it...")
-                    value = r.recognize_google(audio, None, 'en-US', 1, False, False) if connect(fake=True) else r.recognize_vosk(audio, 'en')
+                    value = r.recognize_google(audio, None, language[1][3], 1, False, False) if connect() else r.recognize_vosk(audio, language[1][3], 'models/en_us-small-0.15')
                     printnspeak("You said {}".format(value))
                     return value
                 except speechRecog.UnknownValueError:
@@ -64,17 +72,29 @@ if __name__ == "__main__":
                 except speechRecog.RequestError as e:
                     return "Uh oh! Couldn't request results from Speech Recognition services; {0}".format(e)
                 except speechRecog.ModelNotFoundError as e:
-                    return "Err; {0}".format(e)
+                    return "Aah! Speech Recognition model not found; {0}".format(e)
         except KeyboardInterrupt:
             pass
 
     def greet(time):
+        hour = datetime.datetime.now().hour
+        text = None
+        if time == 'welcome':
+            if hour >= 0 and hour < 12:
+                text = random.choice(("Good Morning!", "Good day to you.", "Have a great day.", "Hello there!", "Wishing you the best for the day ahead.", "How are you this fine morning?", "What a pleasant morning we are having.", "How is your morning going so far?", "Morning!"))
+            if hour >= 12 and hour < 17:
+                text = "Afternoon"
+            elif hour >= 17:
+                text = "Evening"
+        return text
         pass
 
+    
+    printnspeak(greet('welcome'))
     while(RUN):
         q = listen()
         qLower = str(q).lower()
-        if "Oops! Didn't catch that" in str(q) or "Uh oh! Couldn't request results from Speech Recognition services;" in str(q):
+        if "Oops! Didn't catch that" in str(q) or "Uh oh! Couldn't request results from Speech Recognition services;" in str(q) or "Aah! Speech Recognition model not found;" in str(q):
             printnspeak(q)
         else:
             break
